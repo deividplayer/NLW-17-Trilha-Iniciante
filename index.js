0eda6a38d904cb51f2ec99c22ad8239fd414895f
deividplayer <deivisonpnascimento@gmail.com>
2024-09-12 15:10:35 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -17,24 +17,39 @@ console.log(mensagem); */
 
 console.log(metas[1] + ", " + metas[0]) */
 
-let meta = {
+/* let meta = {
     value: 'ler um livro por mês',
     address: 2,
     checked: false,
 /*     isChecked: (info) => {
         console.isChecked(info)
     }, */
-    log: (info) => {
+/*     log: (info) => {
         console.log(info)
-    }
+    } */
 
-}
+/* } */ 
 /* meta.value = "não é mais ler um livro" */
-meta.log(meta.value)
+/* meta.log(meta.value) */
 /* console.log(meta.value) */
 
 
 // function  // arrow function
-const criarMeta = () => {}
+/* const criarMeta = () => {} */
 
 /* function criarMeta() {} */
+
+let meta = {
+    value: 'ler um livro por mês',
+    checked: true,
+}
+
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
+
+console.log(metas[1].value)
